d11c323cdee5b4a5b7bfeec10ea18ecd60bdbdd2
slarge <largesi@gmail.com>
2015-01-11 00:15:58 +0100

    Add .rdata and associated information

---
+++ R/HLrecs.R
@@ -0,0 +1,27 @@
+#' Length-based species information for ICES NS-IBTS surveys from 1965-2014
+#'
+#' A dataset containing length-based species information for ICES North Sea
+#' international bottom trawl survey.
+#'
+#' @format A data frame with 845052 rows and 25 variables:
+#' \describe{
+#'   \item{Survey}{price, in US dollars}
+#'   \item{Quarter}{weight of the diamond, in carats}
+#'   \item{Ship}{price, in US dollars}
+#'   \item{Gear}{weight of the diamond, in carats}
+#'   \item{SweepLngt}{price, in US dollars}
+#'   \item{GearExp}{weight of the diamond, in carats}
+#'   \item{DoorType}{price, in US dollars}
+#'   \item{StNo}{weight of the diamond, in carats}
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#' }
+#' @source \url{https://datras.ices.dk/WebServices/Webservices.aspx}
+"HLrecs"
+
