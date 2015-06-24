583e2741891e890ac2191828f4829759a111f075
slarge <largesi@gmail.com>
2015-06-24 14:09:59 +0200

    Update to cover getHLfun, getHHfun, and getCAfun functionality

--- R/getDATRAS.R
+++ R/getDATRAS.R
@@ -1,107 +1,73 @@
-#' getDATRAS.R
+#' getDATRASfun.R
 
-#' Download files from DATRAS
-#' @param surveyName, startYear, endYear, startQuarter, endQuarter, path = "~/", inParallel
+#' Extracts age data files from DATRAS
+#' @param record, survey, startyear, endyear, startquarter, endquarter, parallel = FALSE, cores = NULL
 #' @return none
-#' @seealso \code{\link{cacheDATRAS}}, a\code{\link{loadDATRAS}}, \code{\link{getCAfun}},
-#' \code{\link{getHLfun}}, and \code{\link{getHHfun}}
+#' @seealso none
 #' @details the update is slow, avoiding straining the server or client.
 #'   please allow this call to run overnight for a complete upgrade.
-#' @keywords download, DATRAS, survey\
+#' @keywords download, DATRAS, survey, age, length
 #' @examples \dontrun{
-#'  getDATRAS(surveyName = "NS-IBTS", startYear = 2005, endYear = 2006, startQuarter = 1,
-#'  endQuarter = 4, path = ".", inParallel = TRUE)
+#'  getDATRAS()
 #' }
 #' @export
-#'
-
-###########################################################################
 #
-getDATRAS <- function(surveyName, startYear, endYear, startQuarter, endQuarter, path = ".", inParallel = FALSE) {
-  # TO do:
-  ## ifelse with the length of data adjusted by the length of data at the web service
-
-
-
-
-  #   if(paste0("allDATRAS_", startYear, "-", endYear, ".Rdata") %in% list.files(path)) {
-#     cat("Hold on!!! \n \n",
-#         paste0("allDATRAS_", startYear, "-", endYear, ".Rdata"),
-#         " is located within ",
-#         path,
-#         "\n Try load(",
-#         paste0("allDATRAS_", startYear, "-", endYear, ".Rdata"),
-#         ") to continue.")
-#
-#   } else {
-    # Reading the data can take some time, so the process is split and saved into 5 yr blocks in case there is a problem
-    # with a connection and the process must be restarted. In that case, change the startYear accordingly.
-    hhFile <- list()
-    hlFile <- list()
-    caFile <- list()
-    tempFolder <- tempdir()
+getDATRAS <- function(record, survey, startyear, endyear, quarters, parallel = FALSE, cores = NULL) {
+  #   library(XML)
+  #   library(doParallel)
+  #   library(parallel)
+  #   library(foreach)
+  #   library(data.table)
+  strt <- Sys.time()
+  #
+  seqYear <- startyear:endyear
+  #
+  if(!record %in% c("HL", "HH", "CA") ) stop("Please specify record type: HH (haul meta-data),
+                                             HL (Species length-based data),
+                                             CA (species age-based data)")
+  getURL <- apply(expand.grid(record, survey, seqYear, quarters),
+                    1,
+                    function(x) paste0("http://datras.ices.dk/WebServices/DATRASWebService.asmx/get",
+                                       x[1],
+                                       "data?survey=", x[2],
+                                       "&year=", x[3],
+                                       "&quarter=", x[4]))
+  #
+  if(parallel == TRUE) {
+    if(missing("cores")) stop("Please specify how many cores you wish to devote to this task.")
     #
-    if(startYear < 1965) stop("startYear must be greater than 1965.")
-    START <- startYear
+    unregister <- function() {
+      env <- foreach:::.foreachGlobals
+      rm(list=ls(name=env), pos=env)
+    } # close unregister
     #
-    while(START <= endYear){
-      END <- ifelse(START + 5 <= endYear, START + 5, (endYear - START) + START)
-      cat("Extracting from ", START, "-", END, "\n")
-      #
-      tHH <- getHHfun(survey = surveyName,
-                      startyear = START,
-                      endyear = END,
-                      startquarter = startQuarter,
-                      endquarter = endQuarter,
-                      parallel = inParallel)
-      hhPath <- paste0(tempFolder, "/HH", START, "-", END, ".Rdat")
-#       hhPath <- paste0("HH_", START, "-", END)
-#       hhPath <- paste0(path, "/HH_", START, "-", END, ".Rdata")
-      save(tHH, file = hhPath)
-      rm(tHH)
-      gc(verbose = TRUE)
-#       assign(hhPath, tHH)
-      hhFile <- rbind(hhFile, hhPath)
-      #
-      tHL <- getHLfun(survey = surveyName,
-                      startyear = START,
-                      endyear = END,
-                      startquarter = startQuarter,
-                      endquarter = endQuarter,
-                      parallel = inParallel)
-      hlPath <- paste0(tempFolder, "/HL", START, "-", END, "Rdat")
-#       hlPath <-  paste0("HL_", START, "-", END)
-#       hlPath <-  paste0(path, "/HL_", START, "-", END, ".Rdata")
-      save(tHL, file = hlPath)
-      rm(tHL)
-      gc(verbose = TRUE)
-#       assign(hlPath, tHL)
-      hlFile <- rbind(hlFile, hlPath)
-      #
-      tCA <- getCAfun(survey = surveyName,
-                      startyear = START,
-                      endyear = END,
-                      startquarter = startQuarter,
-                      endquarter = endQuarter,
-                      parallel = inParallel)
-#       caPath <- paste0("CA_", START, "-", END)
-#       caPath <- paste0(path, "/CA_", START, "-", END, ".Rdata")
-        caPath <- paste0(tempFolder, "/CA", START, "-", END, "Rdat")
-#       assign(caPath, tCA)
-      save(tCA, file = caPath)
-      rm(tCA)
-      gc(verbose = TRUE)
-      caFile <- rbind(caFile, caPath)
-      #
-      START <- END + 1
-    }
-    # Combine 5 year blocks into full length data.frames and return objects
-    HHrecs <- do.call(rbind, lapply(hhFile, function(x) get(load(x))))
-    HLrecs <- do.call(rbind, lapply(hlFile, function(x) get(load(x))))
-    CArecs <- do.call(rbind, lapply(caFile, function(x) get(load(x))))
-
-    unlink(tempFolder, recursive = TRUE)
-    return(list(HHrecs = HHrecs, HLrecs = HLrecs, CArecs = CArecs))
-}
-
-
+    cl <- makeCluster(cores)
+    registerDoParallel(cores = cl)
+    #
+    getDATA <- foreach(temp = getURL,
+                     .combine = function(...) rbindlist(list(...), fill = TRUE),
+                     .multicombine = T,
+                     .inorder = F,
+                     .maxcombine = 1000,
+                     .packages = c("XML", "data.table")) %dopar% {
+                       data.table(t(xmlSApply(xmlRoot(xmlTreeParse(temp, isURL = T, options = HUGE, useInternalNodes =  T)),
+                                              function(x) xmlSApply(x, xmlValue))))
+                     } # close foreach %dopar%
+    stopCluster(cl)
+    unregister()
+  } # close parallel == TRUE
+  #
+  if(parallel == FALSE) {
+    getDATA <- foreach(temp = getURL,
+                     .combine = function(...) rbindlist(list(...), fill = TRUE),
+                     .multicombine=T,
+                     .inorder=F,
+                     .maxcombine=1000,
+                     .packages = c("XML", "data.table")) %do% {
+                       data.table(t(xmlSApply(xmlRoot(xmlTreeParse(temp, isURL = T, options = HUGE, useInternalNodes =  T)),
+                                              function(x) xmlSApply(x, xmlValue))))
+                     } # close foreach %do%
+  } # close parallel == FALSE
+  print(Sys.time()-strt)
+  return(getDATA)
+} # close function
